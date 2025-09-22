3f297d3ac109887940b623f7b9339d3ce4b27881
tanvirhossainrupu500-ux <tanvirhossainrupu500@gmail.com>
2025-09-22 15:44:39 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Starter Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Starter'),
+      ),
+      body: const Center(
+        child: Text('Hello, Flutter!'),
+      ),
+    );
+  }
+}
